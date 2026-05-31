f2487b4644706b38704224cc5c54b63343ef7076
Anustup Maity <ANUSTUPMAITY2004@GMAIL.COM>
2026-05-31 13:12:09 +0530

    src/pages/PaymentPage.jsx

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { collection, addDoc, serverTimestamp, query, where, getDocs } from 'firebase/firestore';
 import { QRCodeSVG } from 'qrcode.react';
